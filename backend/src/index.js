b002a0fc99930f1b61459ddac02f3501d7a12bdd
Govind Suthar <youremail@example.com>
2025-06-01 14:30:14 +0530

    change3

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,27 +4,13 @@ import cors from 'cors'
 import connectRouter from './Routes/connectionRoutes.js';
 import {app, server} from './lib/socket.js'
 
-import path from 'path';
-
-const __dirname = path.resolve() 
-
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }))
 
 app.use(express.json());
 app.use("/connect", connectRouter)
-
-
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
-}
-
 
 server.listen(process.env.PORT, () => {
     console.log("server is running on ", process.env.PORT)
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io'
+import 'dotenv/config'
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL,
     }
 })
 
