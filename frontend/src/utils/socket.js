2bfdfc0af1bf048274a732b15d520c9814a922f7
Govind Suthar <youremail@example.com>
2025-06-01 09:10:44 +0530

    project completed2

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 const socket = io(BASE_URL, {
     autoConnect: false, // Prevent auto-connect
